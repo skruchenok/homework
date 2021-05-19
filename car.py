c340b5fee7e3dd4032098c02c342eb962124c93e
skruchenok <kruchenok_sergei@mail.ru>
2021-05-19 11:18:32 +0300

    foo speed commited

--- car.py
+++ car.py
@@ -7,3 +7,19 @@ class car:
         self.speed = speed
         self.max_speed = max_speed
 
+    def sp(self, value):
+        self.speed += value
+        if self.speed > self.max_speed:
+            self.speed = self.max_speed
+        if self.speed < 0:
+            self.speed = 0
+
+
+car1 = car('Nissan', 'Primera', 'blue', 'petrol', 0, 210)
+car2 = car('Kia', 'Sportage', 'beige', 'petrol', 40, 180)
+# car1.sp(40)
+# print(car1.speed)
+# car1.sp(40)
+# print(car1.speed)
+# car1.sp(-100)
+# print(car1.speed)
