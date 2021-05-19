db66bc019144c8772c141207d982092dbd8c633d
skruchenok <kruchenok_sergei@mail.ru>
2021-05-19 10:40:59 +0300

    class car commited

---
+++ car.py
@@ -0,0 +1,9 @@
+class car:
+    def __init__(self, brand, model, color, engine, speed, max_speed):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.engine = engine
+        self.speed = speed
+        self.max_speed = max_speed
+
